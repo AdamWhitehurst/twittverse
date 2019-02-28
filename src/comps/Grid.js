9a323b3a24541fe8ec7c784f32582f80e6f33d72
AdamWhitehurst <Adam.Whitehurst@live.com>
2019-02-28 15:39:26 -0800

    Clean up

--- src/comps/Grid.js
+++ src/comps/Grid.js
@@ -1,17 +1,15 @@
 import React from "react";
 import { withStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
+import { textChangeRangeNewSpan } from "typescript";
 
 const styles = theme => ({
-  root: {
-    margin: 0,
-    backgroundColor: theme.palette.secondary.main
-  },
-  paper: {
+  grid: {
     padding: theme.spacing.unit * 2,
-    textAlign: "center",
-    color: theme.palette.text.secondary,
-    backgroundColor: theme.palette.primary.main
+    paddingTop: theme.spacing.unit * 8,
+    flexGrow: 1,
+    overflowX: "scroll",
+    backgroundColor: theme.palette.secondary.main
   }
 });
 
@@ -19,17 +17,7 @@ function UnstyledGrid(props) {
   const { classes } = props;
 
   return (
-    <Grid
-      className={classes.root}
-      container
-      direction="row"
-      justify="center"
-      alignItems="center"
-      spacing={24}
-    >
-      {props.children}
-      {props.children}
-      {props.children}
+    <Grid className={classes.grid} container direction="row" spacing={24}>
       {props.children}
     </Grid>
   );
