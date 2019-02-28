61b5ba2140bf29b18639a10538f8bad47eca5552
AdamWhitehurst <Adam.Whitehurst@live.com>
2019-02-28 15:39:48 -0800

    Test override

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -7,11 +7,25 @@ const MUITheme = createMuiTheme({
     },
     secondary: {
       main: "#e0e0e0"
-    },
-    typography: {
-      useNextVariants: true
     }
-  }
+  },
+  overrides: {
+    // Name of the component ⚛️ / style sheet
+    MuiButton: {
+      // Name of the rule
+      text: {
+        // Some CSS
+        background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+        borderRadius: 3,
+        border: 0,
+        color: "white",
+        height: 48,
+        padding: "0 30px",
+        boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)"
+      }
+    }
+  },
+  typography: { useNextVariants: true }
 });
 
 export default MUITheme;
