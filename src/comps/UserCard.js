73059663abc2b6de694d6fe99b13a1c200c028d3
AdamWhitehurst <Adam.Whitehurst@live.com>
2019-03-01 09:42:02 -0800

    Improve tweet formatting

--- src/comps/UserCard.js
+++ src/comps/UserCard.js
@@ -21,49 +21,72 @@ const styles = theme => ({
   }
 });
 
+/**
+ * @function`Renders a card which allows a user to sign into a Twitter account`
+ */
 function UserCard(props) {
   const { classes } = props;
 
-  const renderLoginButton = socket => {
-    return (
-      <StyledCard>
-        <StyledButton onClick={socket.login}>
-          <PersonAddIcon className={classes.leftIcon} />
-          Connect Account
-        </StyledButton>
-      </StyledCard>
+  const textToHTMLLink = text =>
+    // Replace all @ mentions and http text with external links
+    text
+      .replace(
+        /(https?:\/\/[^\s]+)/g,
+        `<a class="external-link" href="javascript:void(0);" onclick="require('electron').shell.openExternal('$&');">$&</a>`
+      )
+      .replace(
+        /[@]([\w]*)/g,
+        `<a class="external-link" href="javascript:void(0);" onclick="require('electron').shell.openExternal('https://twitter.com/$&');">$&</a>`
+      );
+
+  const tweetTextToTypography = tweetText => (
+    // Parse text of tweet into a pretty.
+    <Typography
+      variant="body1"
+      dangerouslySetInnerHTML={{ __html: `${textToHTMLLink(tweetText)}` }}
+    />
+  );
+
+  const renderLoginButton = socket => (
+    //
+    <StyledCard>
+      <StyledButton onClick={socket.login}>
+        <PersonAddIcon className={classes.leftIcon} />
+        Connect Account
+      </StyledButton>
+    </StyledCard>
+  );
+
+  const tweetToListItem = tweet => (
+    // Parse tweet into a pretty.
+    <ListItem key={tweet.id}>{tweetTextToTypography(tweet.full_text)}</ListItem>
+  );
+  const renderTimeline = timeline =>
+    // For each tweet, if retweeted, parse that.
+    timeline.map(tweet =>
+      tweet.retweeted
+        ? tweetToListItem(tweet.retweeted_status)
+        : tweetToListItem(tweet)
     );
-  };
-  const renderTimeline = timeline => {
-    return timeline.map(tweet => (
-      <ListItem key={tweet.id}>
-        <Typography varient="">{tweet.text}</Typography>
-      </ListItem>
-    ));
-  };
-  const renderUser = socket => {
-    return (
-      <StyledCard>
-        <Typography variant="h5">{socket.user.profile.displayName}</Typography>
-        <StyledButton onClick={socket.logout}>
-          <PersonOutlineIcon className={classes.leftIcon} />
-        </StyledButton>
-        {socket.timeline && (
-          <List subheader={<ListItem />}>
-            {renderTimeline(socket.timeline)}
-          </List>
-        )}
-      </StyledCard>
-    );
-  };
+  const renderUser = socket => (
+    <StyledCard>
+      <Typography variant="h5">{socket.user.profile.displayName}</Typography>
+      <StyledButton onClick={socket.logout}>
+        <PersonOutlineIcon className={classes.leftIcon} />
+      </StyledButton>
+      {socket.timeline && (
+        <List subheader={<ListItem />}>{renderTimeline(socket.timeline)}</List>
+      )}
+    </StyledCard>
+  );
 
   return (
     <TwittSocket
-      render={socket => {
-        return socket.user !== null
-          ? renderUser(socket)
-          : renderLoginButton(socket);
-      }}
+      render={socket =>
+        // If !socket.user, user has not signed in
+        // So render login button
+        socket.user !== null ? renderUser(socket) : renderLoginButton(socket)
+      }
     />
   );
 }
