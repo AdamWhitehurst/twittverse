e490632885bee11c35ba3c6ed8f48d0492c4554f
Adam Whitehurst <Adam.Whitehurst@live.com>
2019-03-02 14:31:53 -0800

    Delete config.js

--- src/lib/config.js
+++
@@ -1,9 +0,0 @@
-export const PORT = 8080;
-export const CALLBACK_URL = `http://127.0.0.1:${PORT}/twitter/callback`;
-export const API_URL = `http://127.0.0.1:${PORT}`;
-
-export const TWITTER_CONFIG = {
-  consumerKey: TWITTER_KEY,
-  consumerSecret: TWITTER_SECRET,
-  callbackURL: CALLBACK_URL
-};
